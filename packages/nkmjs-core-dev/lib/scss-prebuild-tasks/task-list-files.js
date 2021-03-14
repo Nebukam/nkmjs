39f61ce6ef3446a279be7106e010c84a143a29e6
Tim <nebukam@gmail.com>
2021-03-14 19:02:30 -0400

    Update task-list-files.js

--- packages/nkmjs-core-dev/lib/scss-prebuild-tasks/task-list-files.js
+++ packages/nkmjs-core-dev/lib/scss-prebuild-tasks/task-list-files.js
@@ -7,10 +7,16 @@ class TaskListFiles extends PrebuildTaskBase {
 
     Process() {
         let dirs = this._args[0].split(`,`),
-            exts = null;
+            exts = null,
+            minus = this._argv.Get(`rm`, null);
+
         if (this._args.length > 1) {
             exts = this._args[1].split(`,`);
             if (exts.length === 0) { exts = null; }
+        }
+
+        if (minus) {
+            minus = minus.split(`,`);
         }
 
         var paths = [];
@@ -25,22 +31,37 @@ class TaskListFiles extends PrebuildTaskBase {
         let value = ``, index = 0;
 
         for (let i = 0, n = paths.length; i < n; i++) {
-            let p = paths[i];
+            let p = paths[i],
+                id = ``;
             if (exts != null && !exts.includes(path.extname(p))) { continue; }
 
-            if (index != 0) { value += `, `; index++; }
-
             if (this._argv.Has(`name-only`)) {
+
                 let bname = path.basename(p),
                     bnameSplit = bname.split(`.`);
+
                 if (bnameSplit.length > 1) {
                     bnameSplit.pop();
                     bname = bnameSplit.join(`.`);
                 }
 
-                value += `"${bname}"`;
+                id = bname;
+
             } else {
-                value += `"${p}"`;
+                id = p;
+
+            }
+
+            if (minus) {
+                for (let m = 0, mn = minus.length; m < mn; m++) {
+                    let rm = minus[m];
+                    id = id.split(rm).join(``);
+                }
+            }
+
+            if (id != ``) {
+                if (index != 0) { value += `, `; index++; }
+                value += `"${id}"`;
             }
         }
 
