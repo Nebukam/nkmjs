965aff384d1d0118909f6d9cad3e4b1557948135
Tim <nebukam@gmail.com>
2021-04-02 18:17:47 -0400

    WIP

---
+++ packages/nkmjs-ui-core/lib/views/flow.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const u = require("@nkmjs/utils");
+const collections = require("@nkmjs/collections");
+const actions = require("@nkmjs/actions");
+
+const UI = require(`../ui`);
+const SIGNAL = require(`../signal`);
+const FLAGS = require(`../flags`);
+
+const View = require(`./view`);
+
+
+class Flow extends View {
+    constructor() { super(); }
+
+    _Init(){
+        super._Init();
+        this._chain = new collections.List();
+        this._currentView = null;
+    }
+
+    Next(p_view){
+        // Push new view in chain and set as current
+    }
+
+    Previous(){
+        // Release current view, set last in chain as current
+    }
+
+    _OnCurrentViewRequestClose(p_view){
+        if(this._currentView === p_view){ this.Previous(); }
+    }
+
+}
+
+module.exports = Flow;
--- packages/nkmjs-ui-core/lib/views/index.js
+++ packages/nkmjs-ui-core/lib/views/index.js
@@ -3,6 +3,7 @@
 module.exports = {
 
     View: require(`./view`),
+    Flow: require(`./flow`),
     
     Layer: require(`./layer`),
     LayerContainer: require(`./layer-container`)
