7012bb134f57851948f380311eb023ae46e37b3d
Tim <nebukam@gmail.com>
2022-04-19 09:05:15 -0400

    Update unsaved-doc-handler.js

--- packages/nkmjs-app-core/lib/helpers/unsaved-doc-handler.js
+++ packages/nkmjs-app-core/lib/helpers/unsaved-doc-handler.js
@@ -37,8 +37,8 @@ class UnsavedDocHandler {
                 title: `Unsaved changes`,
                 message: `There are unsaved changes in <span style="color:var(--col-warning-contrast);">${docName}</span>.\nSave before closing?`,
                 actions: [
-                    { label: `Ignore & Quit`, icon: `clear`, flavor: com.FLAGS.WARNING, trigger: { fn: this._IgnoreAndClose } }, //variant: nkm.ui.FLAGS.FRAME
-                    { label: `Save`, icon: `save`, flavor: ui.FLAGS.CTA, trigger: { fn: this._SaveBeforeClosing } }, //variant: nkm.ui.FLAGS.FRAME
+                    { label: `Don't save and Quit`, icon: `clear`, flavor: com.FLAGS.WARNING, trigger: { fn: this._IgnoreAndClose } }, //variant: nkm.ui.FLAGS.FRAME
+                    { label: `Save and Quit`, icon: `save`, flavor: ui.FLAGS.CTA, trigger: { fn: this._SaveBeforeClosing } }, //variant: nkm.ui.FLAGS.FRAME
                     { label: `Cancel` }
                 ],
                 icon: `warning`,
