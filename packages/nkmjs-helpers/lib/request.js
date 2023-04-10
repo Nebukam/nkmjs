4fdfe75908a3b8456caa4330b769258c28d0278a
Tim <nebukam@gmail.com>
2023-04-10 19:11:26 -0400

    WIP

--- packages/nkmjs-helpers/lib/request.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const ui = require("@nkmjs/ui-core");
-
-module.exports = {
-    //DRAWER: class DRAWER extends ui.REQUEST.OVERLAY { constructor() { } },
-    //BUBBLE: class BUBBLE extends ui.REQUEST.OVERLAY { constructor() { } },
-    //TOASTER: class TOASTER extends ui.REQUEST.OVERLAY { constructor() { } },
-};
