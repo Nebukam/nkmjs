8ee96ad5421f5d063be4324de483c489b8ad90a0
Tim <nebukam@gmail.com>
2023-05-20 21:22:29 -0400

    Update deferred-call.js

--- packages/nkmjs-common/lib/time/deferred-call.js
+++ packages/nkmjs-common/lib/time/deferred-call.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const POOL = require(`../pool`);
 const CallList = require("../helpers/call-list");
 
 class DeferredCall {
@@ -14,12 +15,22 @@ class DeferredCall {
         this._resolveCb = p_resolve;
         this._rejectCb = p_reject || p_resolve;
 
-        this._thenResolve = new CallList();
-        this._thenReject = new CallList();
+        this._thenResolve = null;
+        this._thenReject = null;
 
         this.Resolve = this.Resolve.bind(this);
         this.Reject = this.Reject.bind(this);
 
+    }
+
+    get thenResolve() {
+        if (!this._thenResolve) { this._thenResolve = POOL.Rent(CallList); }
+        return this._thenResolve;
+    }
+
+    get thenReject() {
+        if (!this._thenReject) { this._thenReject = POOL.Rent(CallList); }
+        return this._thenReject;
     }
 
     /**
@@ -31,12 +42,12 @@ class DeferredCall {
     async Call(p_onResolveOrAny = null, p_onReject = null) {
 
         if (p_onResolveOrAny) {
-            this._thenResolve.Add(p_onResolveOrAny);
-            if (!p_onReject) { this._thenReject.Add(p_onResolveOrAny); }
+            this.thenResolve.Add(p_onResolveOrAny);
+            if (!p_onReject) { this.thenReject.Add(p_onResolveOrAny); }
         }
 
         if (p_onReject) {
-            this._thenReject.Add(p_onReject);
+            this.thenReject.Add(p_onReject);
         }
 
         if (!this._promise) {
@@ -56,13 +67,15 @@ class DeferredCall {
     Resolve(p_resolveArg) {
         this._resolve?.(p_resolveArg);
         this._resolveCb?.(p_resolveArg);
-        this._thenResolve.Notify(p_resolveArg).Clear();
+        this._thenResolve?.Notify(p_resolveArg).Release();
+        this._thenResolve = null;
     }
 
     Reject(p_resolveArg) {
         this._reject?.(p_resolveArg);
         this._rejectCb?.(p_resolveArg);
-        this._thenReject.Notify(p_resolveArg).Clear();
+        this._thenReject?.Notify(p_resolveArg).Release();
+        this._thenReject = null;
     }
 
     _Clear() {
