15b2bfe2bbe66a85c8f356aa02140ed5b22d6216
Tim <nebukam@gmail.com>
2023-05-20 21:18:52 -0400

    Update deferred-call.js

--- packages/nkmjs-common/lib/time/deferred-call.js
+++ packages/nkmjs-common/lib/time/deferred-call.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const CallList = require("../helpers/call-list");
+
 class DeferredCall {
 
     constructor(p_init = null, p_resolve = null, p_reject = null) {
@@ -10,14 +12,32 @@ class DeferredCall {
 
         this._initCb = p_init;
         this._resolveCb = p_resolve;
-        this._rejectCb = p_reject;
+        this._rejectCb = p_reject || p_resolve;
+
+        this._thenResolve = new CallList();
+        this._thenReject = new CallList();
 
         this.Resolve = this.Resolve.bind(this);
         this.Reject = this.Reject.bind(this);
-        
+
     }
 
-    async Call() {
+    /**
+     * 
+     * @param {*} p_onResolveOrAny If only callback set, will be called on both resolve AND reject.
+     * @param {*} p_onReject 
+     * @returns 
+     */
+    async Call(p_onResolveOrAny = null, p_onReject = null) {
+
+        if (p_onResolveOrAny) {
+            this._thenResolve.Add(p_onResolveOrAny);
+            if (!p_onReject) { this._thenReject.Add(p_onResolveOrAny); }
+        }
+
+        if (p_onReject) {
+            this._thenReject.Add(p_onReject);
+        }
 
         if (!this._promise) {
             this._promise = new Promise((res, rej) => {
@@ -35,13 +55,14 @@ class DeferredCall {
 
     Resolve(p_resolveArg) {
         this._resolve?.(p_resolveArg);
-        this._resolveCb?.();
+        this._resolveCb?.(p_resolveArg);
+        this._thenResolve.Notify(p_resolveArg).Clear();
     }
 
     Reject(p_resolveArg) {
         this._reject?.(p_resolveArg);
-        if (this._rejectCb) { this._rejectCb(); }
-        else { this._resolveCb?.(); }
+        this._rejectCb?.(p_resolveArg);
+        this._thenReject.Notify(p_resolveArg).Clear();
     }
 
     _Clear() {
